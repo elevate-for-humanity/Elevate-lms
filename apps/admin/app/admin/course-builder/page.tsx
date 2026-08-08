6641c4678b78ff154ca16351dfdb6e5d6de0c0f9
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-08-08 15:54:41 -0400

    chore(legacy): remove parallel nested Course Builder route

--- apps/admin/app/admin/course-builder/page.tsx
+++
@@ -1,7 +0,0 @@
-import UnifiedCourseBuilder from '@/components/admin/course-builder/UnifiedCourseBuilder';
-
-export const dynamic = 'force-dynamic';
-
-export default function CourseBuilderPage() {
-  return <UnifiedCourseBuilder />;
-}

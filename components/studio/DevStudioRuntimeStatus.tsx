8f5b555f3e3cff7ea411cb974b4022f0b0c0b4db
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-08-14 07:35:10 -0400

    fix(studio): point runtime status to canonical studio route

--- components/studio/DevStudioRuntimeStatus.tsx
+++ components/studio/DevStudioRuntimeStatus.tsx
@@ -56,7 +56,7 @@ export default function DevStudioRuntimeStatus({
         </div>
         {!compact && (
           <Link
-            href="/admin/studio"
+            href="/studio"
             className="text-[11px] text-[#4ec9b0] underline"
           >
             Open Services

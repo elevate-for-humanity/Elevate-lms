aa3147d8fbbe5b8fe4d8f4e23f8a78514c671f4e
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-08-15 15:44:12 -0400

    fix(build): use canonical Paris client component directly

--- components/paris/ParisFloatingWrapper.tsx
+++ components/paris/ParisFloatingWrapper.tsx
@@ -1,15 +1,6 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-// Dynamically import ParisFloatingButton to avoid SSR issues
-const ParisFloatingButton = dynamic(
-  () => import('./ParisFloatingButton').then((mod) => mod.ParisFloatingButton),
-  {
-    ssr: false,
-    loading: () => null,
-  }
-);
+import { ParisFloatingButton } from './ParisFloatingButton';
 
 export function ParisFloatingWrapper() {
   return <ParisFloatingButton />;

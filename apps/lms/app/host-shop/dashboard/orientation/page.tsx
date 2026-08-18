ae416afb94dfc68ec3a270a541772f3b3cd61167
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-08-18 17:26:45 -0400

    refactor(routes): remove duplicate Host Shop orientation alias

--- apps/lms/app/host-shop/dashboard/orientation/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export const dynamic = 'force-dynamic';
-
-export default function LegacyHostShopOrientationRoute() {
-  redirect('/host-shop/orientation');
-}

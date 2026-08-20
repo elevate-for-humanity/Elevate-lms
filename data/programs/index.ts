6d6ff3d313470b37f7f4ecadd46891c0ef6023fd
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-08-20 05:38:07 -0400

    fix(seo): register esthetician apprenticeship canonical program

--- data/programs/index.ts
+++ data/programs/index.ts
@@ -37,6 +37,7 @@ import { MEDICAL_ASSISTANT } from './medical-assistant';
 import { COSMETOLOGY } from './cosmetology-apprenticeship';
 import { CNA } from './cna';
 import { ESTHETICIAN } from './esthetician';
+import { ESTHETICIAN_APPRENTICESHIP } from './esthetician-apprenticeship';
 import { NAIL_TECH } from './nail-technician-apprenticeship';
 import { CULINARY } from './culinary-apprenticeship';
 import { SANITATION } from './sanitation-infection-control';
@@ -56,6 +57,7 @@ const STATIC_PROGRAMS: ProgramSchema[] = [
   COSMETOLOGY,
   CNA,
   ESTHETICIAN,
+  ESTHETICIAN_APPRENTICESHIP,
   NAIL_TECH,
   CULINARY,
   SANITATION,

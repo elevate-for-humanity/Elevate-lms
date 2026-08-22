60a2099a110db3b2e2ba51521e9983161cb5cd95
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-08-22 15:21:17 -0400

    refactor(admissions): remove retired provisioning declaration

--- apps/marketing/types/paris-provisioning.d.ts
+++
@@ -1,7 +0,0 @@
-import '@/lib/paris/admissions/provisioning-service';
-
-declare module '@/lib/paris/admissions/provisioning-service' {
-  interface ProvisioningResult {
-    credentialId?: string;
-  }
-}

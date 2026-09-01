a7a588b2ce816a9172c2dd1917775fbc846d2f25
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-09-01 18:16:50 +0000

    Centralize official WorkOne booking routes

---
+++ lib/workone/booking.ts
@@ -0,0 +1,12 @@
+/**
+ * Official WorkOne Indy scheduling routes.
+ *
+ * WorkOne owns and operates this Acuity calendar. It is intentionally separate
+ * from Elevate calendars and must not inherit Elevate's testing or consultation
+ * lead-time rules.
+ */
+export const WORKONE_INDY_BOOKING_URL =
+  'https://workoneindy.as.me/schedule/e8f310c0/appointment/91381838';
+
+export const WORKONE_INDY_DIRECTORY_URL =
+  'https://www.in.gov/dwd/workone/workone-locations/';

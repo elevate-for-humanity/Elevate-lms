4b1e1702f7df30e00dd70951238f6984c67461ce
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-09-05 06:52:44 +0000

    Add accounting pathway hero to bookkeeping page

--- components/programs/ProgramDetailPage.tsx
+++ components/programs/ProgramDetailPage.tsx
@@ -199,6 +199,7 @@ export default function ProgramDetailPage({
                   <HeroPicture
                     src={heroPosterSrc}
                     alt={heroAlt}
+                    preserveAspectRatio={p.slug === 'bookkeeping'}
                     microLabel={banner.microLabel}
                     analyticsName={banner.analyticsName}
                     belowHeroHeadline={safeHeadline}

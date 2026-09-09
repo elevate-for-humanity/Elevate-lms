427dec20d607db66d54b71c5cb6d0cfc3398459f
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-09-09 05:57:39 -0400

    Add accounting pathway artwork to bookkeeping hero

--- components/programs/ProgramDetailPage.tsx
+++ components/programs/ProgramDetailPage.tsx
@@ -202,6 +202,7 @@ export default function ProgramDetailPage({
                   <HeroPicture
                     src={heroPosterSrc}
                     alt={heroAlt}
+                    preserveAspectRatio={p.slug === 'bookkeeping'}
                     microLabel={banner.microLabel}
                     analyticsName={banner.analyticsName}
                     belowHeroHeadline={safeHeadline}

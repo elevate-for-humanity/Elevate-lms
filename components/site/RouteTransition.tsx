d4400afa54d165db8408910793dd38c88a4b8469
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-09-10 08:41:52 -0400

    perf(marketing): render homepage without delayed route fade

--- components/site/RouteTransition.tsx
+++ components/site/RouteTransition.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation';
 export function RouteTransition({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   return (
-    <div key={pathname} className="route-transition">
+    <div key={pathname} className={pathname === '/' ? undefined : 'route-transition'}>
       {children}
     </div>
   );

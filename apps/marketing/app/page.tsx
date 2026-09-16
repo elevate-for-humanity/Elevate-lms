d7db049db3dfdac2a1c3ed28a2b5c28007d6d81e
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-09-16 18:53:59 -0400

    Reorder homepage around apprenticeships and employers

--- apps/marketing/app/page.tsx
+++ apps/marketing/app/page.tsx
@@ -91,16 +91,16 @@ export default function HomePage() {
       <StructuredData />
       <main className="[&_a]:no-underline [&_a:hover]:no-underline">
         <PlatformHubHero />
-        <div data-narration-disabled="true">
-          <HomeFunding />
-        </div>
+        <div data-narration-disabled="true"><HomeBeautyPriority /></div>
+        <div data-narration-disabled="true"><HomeFeaturedHostShop /></div>
         <div data-narration-disabled="true">
           <HomeCareerPathways />
         </div>
-        <div data-narration-disabled="true"><HomePlatformOverview /></div>
         <div data-narration-disabled="true"><HomeEmployerStrip /></div>
-        <div data-narration-disabled="true"><HomeFeaturedHostShop /></div>
-        <div data-narration-disabled="true"><HomeBeautyPriority /></div>
+        <div data-narration-disabled="true">
+          <HomeFunding />
+        </div>
+        <div data-narration-disabled="true"><HomePlatformOverview /></div>
         <div data-narration-disabled="true">
           <HomeFinalCTA />
         </div>

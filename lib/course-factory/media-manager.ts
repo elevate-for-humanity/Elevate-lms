663e07a24497b2a908d3231a96f6ecda1d1c74af
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-09-16 23:53:58 -0400

    Prevent valid long lesson renders from being recycled (#1145)
    * Fix long lesson render lease policy
    
    * Use long render lease for video worker
    
    * Extend video heartbeat lease for long renders

--- lib/course-factory/media-manager.ts
+++ lib/course-factory/media-manager.ts
@@ -5,7 +5,8 @@ import { classifyVideoFailure, type VideoAssetKind, type VideoJob } from '@/lib/
 import { MEDIA_QUALITY_GATE_VERSION, mediaQualityFailures, type MediaQualityEvidence } from '@/lib/video/media-quality-gate';
 
 export const COURSE_MEDIA_MAX_RETRIES = 3;
-export const COURSE_MEDIA_STALE_RENDER_MS = 45 * 60 * 1000;
+export const COURSE_MEDIA_STALE_RENDER_MS = 2 * 60 * 60 * 1000;
+export const COURSE_MEDIA_RENDER_LEASE_SECONDS = 2 * 60 * 60;
 export const COURSE_MEDIA_RETRY_BASE_MS = 60 * 1000;
 export const COURSE_MEDIA_RETRY_MAX_BACKOFF_MS = 15 * 60 * 1000;
 

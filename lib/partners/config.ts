5d810329ecbaa135506d6b29088c6f90acddb713
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-09-24 06:03:04 -0400

    remove discontinued HSI partnership references

--- lib/partners/config.ts
+++ lib/partners/config.ts
@@ -26,12 +26,6 @@ export function getPartnerConfig(partner: PartnerType): PartnerConfig {
   };
 
   switch (partner) {
-    case 'hsi':
-      config.baseUrl = process.env.HSI_API_BASE_URL || '';
-      config.apiKey = process.env.HSI_API_KEY;
-      config.apiSecret = process.env.HSI_API_SECRET;
-      config.orgId = process.env.HSI_ORGANIZATION_ID;
-      break;
 
     case 'certiport':
       // Certiport is portal-based (no API). Exams proctored on-site via Compass.
